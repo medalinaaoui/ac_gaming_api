d1bb2c36f0200e3f802c6dacb2c8aa008d128992
Med Ali Naaoui <chayga00@gmail.com>
2023-11-09 18:14:33 +0100

    deploy

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["http://localhost:5173", "http://localhost:3000"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:3000",
+  "https://ac-gaming-client-med-ali.vercel.app/",
+];
 
 export default allowedOrigins;
